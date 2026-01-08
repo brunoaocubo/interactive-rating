25d4b83a9985a5d04495c27038a5cd014bf71626
brunoaocubo <116372081+brunoaocubo@users.noreply.github.com>
2026-01-08 16:35:56 -0300

    Att: Projeto finalizado
    Versão final da proposta estabelecida foi atendida, um bom foco na lógica do javascript e alterações no dom. Experiência fundamental com certeza.

--- js/script.js
+++ js/script.js
@@ -1,10 +1,27 @@
+const buttons = document.querySelectorAll('.rating')
+var buttonClicked = 0;
+const orange500 = 'var(--Orange500)'
 
+buttons.forEach(element => {
+    element.addEventListener('click', function(event) {
+        //window.alert(event.target.textContent)
+        buttonClicked = event.target.textContent;
+        if(element.style.backgroundColor != orange500){
+            for(let i = 0; i < buttons.length; i++){
+                buttons[i].style.backgroundColor = '';
+                buttons[i].style.color = '';
+            }
+            element.style.backgroundColor = 'var(--Orange500)';  
+            element.style.color = 'var(--Grey950)'
+        }     
+    })
+})
 
 function Avaliar(){
-    var teste = document.getElementsByClassName('rating')
-    let buttons = []
-
-    for(let button = 0; button < teste.length; button++){
-        window.alert(teste[button].value)
-    }
+    var cardRating= document.querySelector('#card-rating')
+    var cardResult = document.querySelector('#card-result-rating')
+    var ratingResult = document.querySelector('#rating-result')
+    cardRating.style.display = 'none'
+    cardResult.style.display = 'flex'
+    ratingResult.innerHTML = buttonClicked
 }
